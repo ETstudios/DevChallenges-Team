21f75560e09ec8406d4e3d8f019994985979f557
Eric Thomas <edt514@gmail.com>
2021-05-08 01:02:58 -0400

    Bring in design elements

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+   
+?>
+<!doctype html>
+<html lang="en">
+<head>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta name="author" content="Eric Thomas">
+   <meta name="description" content="Eric Thomas recreation of the DevChallenges Team page challenge">
+   <meta name="keywords" content="404, error, devchallenges">
+   <title> Creative Team </title>
+    
+   <link rel="shortcut icon" href="img/icon.png">    
+   <link rel="stylesheet" href="css/bootstrap.min.css">
+   <link rel="stylesheet" href="css/team.css">
+   
+   <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
