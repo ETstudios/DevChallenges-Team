95c3eb101e2d1c776d0aeec7d1c9f2435f237894
Eric Thomas <edt514@gmail.com>
2021-05-08 02:51:08 -0400

    Initial layout

--- index.php
+++ index.php
@@ -18,6 +18,134 @@
    <script src="js/bootstrap.min.js"></script>
 </head>
 <body>
-    
+    <div class="container">
+        <header class="row">
+            <br>
+            <div class="col-sm-12 col-md-2"> &nbsp; </div>
+            <div class="col-sm-12 col-md-4">
+              <h3>
+                  The creative crew
+              </h3>
+           </div>
+           <div class="col-sm-12 col-md-3">
+               <br>
+               <strong>
+                   Who We Are
+               </strong>
+               <br><br>
+               <p>
+                    We are a team of creatively diverse, driven, innovative individuals working in various locations from the world.
+               </p>
+           </div>
+           <div class="col-sm-12 col-md-3"> &nbsp; </div>
+        </header>
+        <div class="clear"> &nbsp; </div>
+        <section class="row">
+            <div class="col-xs-6 col-md-4 member">
+                <div>
+                    <div class="col-sm-10">
+                        <img src="img/bill.png" alt="Bill Mahoney portrait">
+                    </div>
+                    <div class="col-sm-2 position">
+                        <p>
+                            Product Owner
+                        </p>
+                    </div>
+                    <div class="col-sm-12 name">
+                        <p>
+                            Bill Mahoney
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-6 col-md-4 member">
+                <div>
+                    <div class="col-sm-10">
+                        <img src="img/saba.png" alt="Saba Cabrera portrait">
+                    </div>
+                    <div class="col-sm-2 position">
+                        <p>
+                            Art Director
+                        </p>
+                    </div>
+                    <div class="col-sm-12 name">
+                        <p>
+                            Saba Cabrera
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-6 col-md-4 member">
+                <div>
+                    <div class="col-sm-10">
+                        <img src="img/shae.png" alt="Shae Le portrait">
+                    </div>
+                    <div class="col-sm-2 position">
+                        <p>
+                            Tech Lead
+                        </p>
+                    </div>
+                    <div class="col-sm-12 name">
+                        <p>
+                            Shae Le
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-6 col-md-4 member">
+                <div>
+                    <div class="col-sm-10">
+                        <img src="img/skylah.png" alt="Skylah Lu portrait">
+                    </div>
+                    <div class="col-sm-2 position">
+                        <p>
+                            UX Designer
+                        </p>
+                    </div>
+                    <div class="col-sm-12 name">
+                        <p>
+                            Skylah Lu
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-6 col-md-4 member">
+                <div>
+                    <div class="col-sm-10">
+                        <img src="img/griff.png" alt="Griff Richards portrait">
+                    </div>
+                    <div class="col-sm-2 position">
+                        <p>
+                            Developer
+                        </p>
+                    </div>
+                    <div class="col-sm-12 name">
+                        <p>
+                            Griff Richards
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-6 col-md-4 member">
+                <div>
+                    <img src="img/stan.png" alt="Stan John portrait">
+                    <p class="position">
+                        Developer
+                    </p>
+                    <div class="col-sm-12 name">
+                        <p>
+                            Stan John
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <div class="clear"> &nbsp; </div>
+        <footer class="row">
+            <div class="col-sm-12 center-text">
+                created by <a href="https://github.com/ETstudios" target="_blank">FaeWilds</a> - <a href="https://devchallenges.io/" target="_blank">devChallenges.io</a>
+            </div>
+        </footer>
+    </div>
 </body>
 </html>
