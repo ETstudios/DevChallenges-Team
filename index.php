f7ddfa7c5a972124f94ea43310843060d77ebd6e
Eric Thomas <edt514@gmail.com>
2021-05-10 16:49:40 -0400

    Update meta tags

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <meta name="author" content="Eric Thomas">
    <meta name="description" content="Eric Thomas recreation of the DevChallenges Team page challenge">
-   <meta name="keywords" content="404, error, devchallenges">
+   <meta name="keywords" content="team, creative crew, devchallenges">
    <title> Creative Team </title>
     
    <link rel="shortcut icon" href="img/icon.png">    
